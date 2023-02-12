20b2f66be0dec6b16b77362d1325f2d883d32acd
andrei-by <14@amail.by>
2023-02-12 23:00:44 +0300

    Update ViewController.swift

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     }
     
     @objc func handleShowLabel() {
-        hiddeLabel.alpha = 0.0
+        hiddeLabel.alpha = 0.2
         self.hiddeLabel.isHidden = false
         UIView.animate(withDuration: 2.0, delay: 0.0) {
             self.hiddeLabel.alpha = 0.9
